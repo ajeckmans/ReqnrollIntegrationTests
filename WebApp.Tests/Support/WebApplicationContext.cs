595f8dea9f0e0033396a85068a488426d5846e7d
Alexander <2088777+ajeckmans@users.noreply.github.com>
2024-08-08 21:30:18 +0200

    added comments to WebApplicationContext.cs

--- WebApp.Tests/Support/WebApplicationContext.cs
+++ WebApp.Tests/Support/WebApplicationContext.cs
@@ -2,10 +2,14 @@
 
 namespace WebApp.Tests.Support;
 
+/// <summary>
+/// A wrapper for the web application factory to ease making requests and verifying the responses.
+/// </summary
 public class WebApplicationContext(WebApplicationFactory<Program> webApp)
 {
     public void Start()
     {
+        // Creating a client actually starts the web application as well
         Client = webApp.CreateClient();
     }
 
@@ -13,6 +17,7 @@ public class WebApplicationContext(WebApplicationFactory<Program> webApp)
 
     public async Task GetAsync(string url)
     {
+        // make the request and store the response body
         var responseMessage =  await Client.GetAsync(url);
 
         BodyOfLastResponse = await responseMessage.Content.ReadAsStringAsync();
